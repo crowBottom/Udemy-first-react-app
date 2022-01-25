1b4093705bbaf9c4a892a34b562e937b354e2dad
crowBottom <jpcunningham_dev@protonmail.com>
2022-01-25 13:26:05 -0600

    First commit for basic react app

---
+++ src/FavoriteFoods.js
@@ -0,0 +1,15 @@
+
+const FavoriteFoods = () => {
+  return (
+    <div>
+      <h1>My Favorite Foods</h1>
+      <ul>
+        <li>Spicy Noodles</li>
+        <li>Toast</li>
+        <li>Coffee</li>
+      </ul>
+    </div>
+  )
+}
+
+export default FavoriteFoods;
